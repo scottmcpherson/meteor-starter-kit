580305432901c1e5dcd1f70df5c49d3b9360766c
scottmcpherson <scott_fl@me.com>
2016-08-03 20:02:53 -0400

    Call client.mutate directly

--- imports/ui/Tasks.js
+++ imports/ui/Tasks.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-apollo';
+import ApolloClient from 'apollo-client';
 import gql from 'graphql-tag';
+
+const client = new ApolloClient();
 
 class TaskItem extends Component {
   constructor(props) {
@@ -9,18 +12,42 @@ class TaskItem extends Component {
   }
   deleteTask() {
     console.log(`Delete ${this.props.task.title}`);
-    this.props.onDelete(this.props.task)
+    // this.props.onDelete(this.props.task)
+    const task = this.props.task;
+    client.mutate({
+      mutation: gql`
+        mutation ($id: String!) {
+          deleteTask(_id: $id) {
+            _id
+            title
+          }
+        }
+      `,
+      variables: {
+        id: task._id
+      },
+      updateQueries: {
+        tasks: (previousQueryResult, { mutationResult }) => {
+          console.log('previousQueryResult: ', previousQueryResult);
+          console.log('mutationResult: ', mutationResult);
+          const newTasks = mutationResult.data.deleteTask;
+          return {
+            tasks: newTasks,
+          }
+        }
+      }
+    })
   }
   render() {
     const { title } = this.props.task;
 
     return (
-      <li>{title} <a href="#" onClick={this.deleteTask}>Delete</a></li>
+      <li>{title}<a href="#" onClick={this.deleteTask}>Delete</a></li>
     )
   }
 }
 
-function TaskList({ tasksData, mutations }) {
+function TaskList({ tasksData }) {
   const { tasks } = tasksData
   console.log(tasks);
 
@@ -30,7 +57,7 @@ function TaskList({ tasksData, mutations }) {
       <ul>
       {tasks &&
         tasks.map(task =>
-          <TaskItem key={task._id} task={task} onDelete={mutations.deleteTask} />
+          <TaskItem key={task._id} task={task} />
         )
       }
       </ul>
@@ -54,31 +81,31 @@ const Tasks = connect({
       }
     };
   },
-  mapMutationsToProps: () => ({
-    deleteTask: (task) => ({
-      mutation: gql`
-        mutation ($id: String!) {
-          deleteTask(_id: $id) {
-            _id
-            title
-          }
-        }
-      `,
-      variables: {
-        id: task._id
-      },
-      updateQueries: {
-        tasks: (previousQueryResult, { mutationResult }) => {
-          console.log('previousQueryResult: ', previousQueryResult);
-          console.log('mutationResult: ', mutationResult);
-          const newTasks = mutationResult.data.deleteTask;
-          return {
-            tasks: newTasks,
-          }
-        }
-      }
-    })
-  })
+  // mapMutationsToProps: () => ({
+  //   deleteTask: (task) => ({
+  //     mutation: gql`
+  //       mutation ($id: String!) {
+  //         deleteTask(_id: $id) {
+  //           _id
+  //           title
+  //         }
+  //       }
+  //     `,
+  //     variables: {
+  //       id: task._id
+  //     },
+  //     updateQueries: {
+  //       tasks: (previousQueryResult, { mutationResult }) => {
+  //         console.log('previousQueryResult: ', previousQueryResult);
+  //         console.log('mutationResult: ', mutationResult);
+  //         const newTasks = mutationResult.data.deleteTask;
+  //         return {
+  //           tasks: newTasks,
+  //         }
+  //       }
+  //     }
+  //   })
+  // })
 })(TaskList);
 
 export default Tasks;
